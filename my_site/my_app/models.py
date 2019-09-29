84e45511c1a83f8b1e59f2f60118837b0680333b
Pham Hoang <iloveyou@Phams-MacBook-Pro.local>
2019-09-29 13:44:56 +0700

    Adding Lecture and Homework models

--- my_site/my_app/models.py
+++ my_site/my_app/models.py
@@ -1,22 +1,31 @@
 from django.db import models
 from django.utils import timezone
+
 # Create your models here.
 
 
-class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    created_date = models.DateTimeField(default=timezone.now())
-    title = models.CharField(max_length=256)
+class Lecture(models.Model):
+    """
+    This model is to store lecture key notes  for student"""
+    tutor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=60)
+    description = models.CharField( max_length=256)
     text = models.TextField()
-    published_date = models.DateTimeField(null=True, blank=True)
-
+    created_date = models.DateField( auto_now=True, auto_now_add=False)   
+    published_date = models.DateField( auto_now=False, auto_now_add=False)
+    published = models.BooleanField(default=False)
+    def publish(self):
+        published = True
+        published_date = timezone.now()
     def __str__(self):
         return self.title
 
 
-class Comment(models.Model):
-    post = models.ForeignKey('Post', on_delete=models.CASCADE)
-    comment = models.TextField()
-    published_date = models.DateTimeField(blank=True, null=True)
-    published = models.BooleanField(default=False)
+class Homework(models.Model):
+    """Model definition for Homework."""
+    lecture = models.ForeignKey('my_app.Lecture', related_name = 'homeworks', on_delete=models.CASCADE)
+    student = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     
+
+    def __str__(self):
+       return self.title + ' ' + student.username + 'homework' 
